80ff6d9b7aa1e207c60b30f639df55088dadfe0a
muhamedRedzic <muhamed.r@netplus.ch>
2015-12-17 16:01:04 +0100

    google maps

--- app/src/main/java/ch/hevs/aipu/customer/MapsActivity.java
+++ app/src/main/java/ch/hevs/aipu/customer/MapsActivity.java
@@ -2,6 +2,7 @@ package ch.hevs.aipu.customer;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,21 +25,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
         LatLng unil = new LatLng(46.522461, 6.5750257);
         mMap.addMarker(new MarkerOptions().position(unil).title("UNIL Lausanne"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(unil,15));
