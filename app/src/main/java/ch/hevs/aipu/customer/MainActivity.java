d11626312d7495fdd809494e8db8d4e4d83be244
muhamedRedzic <muhamed.r@netplus.ch>
2016-01-04 13:39:47 +0100

    moving image in planactivity

--- app/src/main/java/ch/hevs/aipu/customer/MainActivity.java
+++ app/src/main/java/ch/hevs/aipu/customer/MainActivity.java
@@ -7,14 +7,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private Menu menu;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
