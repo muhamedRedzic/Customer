6974b9224255349a5454118d5ea3972f3eecdddc
muhamedRedzic <muhamed.r@netplus.ch>
2016-01-04 10:00:04 +0100

    activities

--- app/src/main/java/ch/hevs/aipu/customer/MainActivity.java
+++ app/src/main/java/ch/hevs/aipu/customer/MainActivity.java
@@ -21,14 +21,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -68,6 +60,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void maps(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void conferences(View view) {
+        Intent intent = new Intent(this, ConferencesActivity.class);
+        startActivity(intent);
+    }
+
+    public void plan(View view) {
+        Intent intent = new Intent(this, PlanActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/ch/hevs/aipu/customer/PlanActivity.java
+++ app/src/main/java/ch/hevs/aipu/customer/PlanActivity.java
@@ -7,15 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class AboutActivity extends AppCompatActivity {
+public class PlanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_plan);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
